59a2ab18c20bd6cd18b30374f1b18efc9049d0d1
ethemcastle <ethemkalaja15@gmail.com>
2022-04-05 22:15:40 +0200

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === "development" ? `http://localhost:8081` : `https://short-url-nyztj.ondigitalocean.app`
+// const baseURL = process.env.NODE_ENV === "development" ? `http://localhost:8081` : `https://short-url-nyztj.ondigitalocean.app`
+const baseURL = `https://short-url-nyztj.ondigitalocean.app`
 const api = axios.create({baseURL});
 
 export const add = async (url: string): Promise<string> => {
